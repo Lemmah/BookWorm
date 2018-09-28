3c611db37b3d627ea48e490681b5abe7cd4d4131
Lemmah <lemayiannakolah@gmail.com>
2018-09-28 22:47:46 +0300

    chore(package): install & setup mongoose
    - add mongoose to project dependecies.
    - require it in the app file.
    - create a connection to the mongodb.
    - listen for and log errors to the console.
    
    [Deliver #setUpMongoose]

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+
+// mongdb connection
+mongoose.connect('mongodb://localhost:27017/bookworm', { useNewUrlParser: true });
+const db = mongoose.connection;
+// mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // parse incoming requests
 app.use(bodyParser.json());
