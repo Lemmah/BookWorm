5c8afa271fc1fb9f1e6171bbb3718fbc9c7fdf4c
Lemmah <lemayiannakolah@gmail.com>
2018-09-28 22:51:30 +0300

    feat(register): add registration functionality
    - create a registration form template.
    - add endpoints for getting and posting the registration post.
    - add a users model for users information.
    - create a user schema for the user data.
    - wire up everything to post the registration data to mongo.
    
    [Delivers #addRegistration]

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 // GET /
 router.get('/', (req, res, next) => {
@@ -14,6 +15,50 @@ router.get('/about', (req, res, next) => {
 // GET /contact
 router.get('/contact', (req, res, next) => {
   return res.render('contact', { title: 'Contact' });
+});
+
+// GET /register
+router.get('/register', (req, res, next) => {
+  return res.render('register', {title: 'Sign Up'});
+});
+
+// POST /register
+router.post('/register', (req, res, next) => {
+  if (
+    req.body.email &&
+    req.body.favoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword
+  ) {
+    // Confirm user typed same password twice
+    if (req.body.password !== req.body.confirmPassword) {
+      const err = new Error('Passwords do not match!');
+      err.status = 400;
+      return next(err);
+    }
+
+    // craete object with form data
+    const userData = {
+      email: req.body.email,
+      name: req.body.name,
+      favoriteBook: req.body.favoriteBook,
+      password: req.body.password
+    };
+
+    // use schema's create method to insert document into Mongo
+    User.create(userData, (error, user) => {
+      if (error) {
+        return next(error);
+      } else {
+        return res.redirect('/profile');
+      }
+    });
+
+  } else {
+    const err = new Error('All fields are required.');
+    err.status = 400;
+    return next(err);
+  }
 });
 
 module.exports = router;
