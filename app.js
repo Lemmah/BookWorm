805f69645a67089fa62dd67774012f249e352ef3
Lemmah <lemayiannakolah@gmail.com>
2018-09-29 17:31:58 +0300

    feat(navbar): customize view for logged in users
    - make userId available in the template as currentUser.
    - check if currentUser is truthy meaning they're logged in.
    - use conditional in template to create a different look if logged in.
    
    [Delivers #loggedInView]

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(session({
   saveUninitialized: false
 }));
 
+// make user ID available in templates
+app.use((req, res, next) => {
+  res.locals.currentUser = req.session.userId;
+  next();
+})
+
 // mongdb connection
 mongoose.connect('mongodb://localhost:27017/bookworm', { useNewUrlParser: true });
 const db = mongoose.connection;
